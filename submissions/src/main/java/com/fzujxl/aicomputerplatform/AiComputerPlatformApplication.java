02a0d05401e6f5295f416c552bbc406597c9a5d6
fzu-jxl <1954754845@qq.com>
2026-05-30 02:48:51 +0800

    Add files via upload
    feat:完成注册登录功能 - 姜欣玲（后端）

---
+++ submissions/src/main/java/com/fzujxl/aicomputerplatform/AiComputerPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.fzujxl.aicomputerplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiComputerPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiComputerPlatformApplication.class, args);
+    }
+
+}
---
+++ submissions/src/test/java/com/fzujxl/aicomputerplatform/AiComputerPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fzujxl.aicomputerplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiComputerPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
